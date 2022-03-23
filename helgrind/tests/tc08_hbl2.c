f8b21b754f8db4c469be06cf11998a890cb21f00
Feiyang Chen <chenfeiyang@loongson.cn>
2022-03-23 11:31:59 +0800

    Add support for loongarch64-linux
    Add support for loongarch64-linux platform. Enable this target in
    the build system.
    
    Signed-off-by: Feiyang Chen <chenfeiyang@loongson.cn>

--- helgrind/tests/tc08_hbl2.c
+++ helgrind/tests/tc08_hbl2.c
@@ -36,6 +36,7 @@
 #undef PLAT_s390x_linux
 #undef PLAT_mips32_linux
 #undef PLAT_mips64_linux
+#undef PLAT_loongarch64_linux
 #undef PLAT_x86_solaris
 #undef PLAT_amd64_solaris
 
@@ -71,6 +72,8 @@
 #endif
 #elif defined(__linux__) && defined(__nanomips__)
 #  define PLAT_nanomips_linux 1
+#elif defined(__linux__) && defined(__loongarch__) && (__loongarch_grlen == 64)
+#  define PLAT_loongarch64_linux 1
 #elif defined(__sun__) && defined(__i386__)
 #  define PLAT_x86_solaris 1
 #elif defined(__sun__) && defined(__x86_64__)
@@ -154,6 +157,11 @@
       : /*out*/ : /*in*/ "r"(&(_lval))              \
       : /*trash*/ "$t0", "$t1", "memory"            \
    )
+#elif defined(PLAT_loongarch64_linux)
+   /* TODO Implement. */
+#include <assert.h>
+#  define INC(_lval,_lqual)                         \
+     assert(0);
 #else
 #  error "Fix Me for this platform"
 #endif
