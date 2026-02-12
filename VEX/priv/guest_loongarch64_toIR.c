CSharperMantle/valgrind-loongarch64

commit 891fffe3f6cd7234d725b205ccea57d5fa7d3c61

Implement lifting for [x]vh{add,sub}w.?[u].?[u]

VEX/priv/guest_loongarch64_toIR.c
@@ -8079,6 +8079,105 @@ static Bool gen_xvpcnt ( DisResult* dres, UInt insn,
    return True;
 }
 
+static Bool gen_vhaddw_vhsubw ( DisResult* dres, UInt insn,
+                                const VexArchInfo* archinfo,
+                                const VexAbiInfo*  abiinfo )
+{
+   UInt vd    = SLICE(insn, 4, 0);
+   UInt vj    = SLICE(insn, 9, 5);
+   UInt vk    = SLICE(insn, 14, 10);
+   UInt insSz = SLICE(insn, 16, 15);
+   UInt isSub = SLICE(insn, 17, 17);
+   UInt isU   = SLICE(insn, 19, 19);
+
+   IRTemp tmpOd = newTemp(Ity_V128);
+   IRTemp tmpEv = newTemp(Ity_V128);
+
+   IROp widenOp = isU ? mkV128EXTHTU(insSz) : mkV128EXTHTS(insSz);
+   IROp mathOp  = isSub ? mkV128SUB(insSz + 1) : mkV128ADD(insSz + 1);
+   UInt szId    = isU ? (insSz + 4) : insSz;
+
+   assign(tmpOd, unop(widenOp,
+                      binop(mkV128PACKOD(insSz), getVReg(vj), mkV128(0x0000))));
+   assign(tmpEv, unop(widenOp,
+                      binop(mkV128PACKEV(insSz), getVReg(vk), mkV128(0x0000))));
+
+   const HChar* nm[2]       = {"vhaddw", "vhsubw"};
+   const HChar* insSizeFrom = mkInsSize(szId);
+   const HChar* insSizeTo =
+      (szId == 3 /* d */) ? "q"
+                          : ((szId == 7 /* du */) ? "qu" : mkInsSize(szId + 1));
+
+   DIP("%s.%s.%s %s, %s, %s\n", nm[isSub], insSizeTo, insSizeFrom, nameVReg(vd),
+       nameVReg(vj), nameVReg(vk));
+
+   STOP_ILL_IF_NO_HWCAP(VEX_HWCAPS_LOONGARCH_LSX);
+
+   putVReg(vd, binop(mathOp, mkexpr(tmpOd), mkexpr(tmpEv)));
+
+   return True;
+}
+
+static Bool gen_xvhaddw_xvhsubw ( DisResult* dres, UInt insn,
+                                  const VexArchInfo* archinfo,
+                                  const VexAbiInfo*  abiinfo )
+{
+   UInt xd    = SLICE(insn, 4, 0);
+   UInt xj    = SLICE(insn, 9, 5);
+   UInt xk    = SLICE(insn, 14, 10);
+   UInt insSz = SLICE(insn, 16, 15);
+   UInt isSub = SLICE(insn, 17, 17);
+   UInt isU   = SLICE(insn, 19, 19);
+
+   IRTemp j       = newTemp(Ity_V256);
+   IRTemp k       = newTemp(Ity_V256);
+   IRTemp jHi     = IRTemp_INVALID;
+   IRTemp jLo     = IRTemp_INVALID;
+   IRTemp kHi     = IRTemp_INVALID;
+   IRTemp kLo     = IRTemp_INVALID;
+   IRTemp tmpOdHi = newTemp(Ity_V128);
+   IRTemp tmpOdLo = newTemp(Ity_V128);
+   IRTemp tmpEvHi = newTemp(Ity_V128);
+   IRTemp tmpEvLo = newTemp(Ity_V128);
+   IRTemp resHi   = newTemp(Ity_V128);
+   IRTemp resLo   = newTemp(Ity_V128);
+   assign(j, getXReg(xj));
+   assign(k, getXReg(xk));
+   breakupV256toV128s(j, &jHi, &jLo);
+   breakupV256toV128s(k, &kHi, &kLo);
+
+   IROp widenOp = isU ? mkV128EXTHTU(insSz) : mkV128EXTHTS(insSz);
+   IROp mathOp  = isSub ? mkV128SUB(insSz + 1) : mkV128ADD(insSz + 1);
+   UInt szId    = isU ? (insSz + 4) : insSz;
+
+   assign(tmpOdLo, unop(widenOp, binop(mkV128PACKOD(insSz), mkexpr(jLo),
+                                       mkV128(0x0000))));
+   assign(tmpEvLo, unop(widenOp, binop(mkV128PACKEV(insSz), mkexpr(kLo),
+                                       mkV128(0x0000))));
+   assign(tmpOdHi, unop(widenOp, binop(mkV128PACKOD(insSz), mkexpr(jHi),
+                                       mkV128(0x0000))));
+   assign(tmpEvHi, unop(widenOp, binop(mkV128PACKEV(insSz), mkexpr(kHi),
+                                       mkV128(0x0000))));
+
+   assign(resLo, binop(mathOp, mkexpr(tmpOdLo), mkexpr(tmpEvLo)));
+   assign(resHi, binop(mathOp, mkexpr(tmpOdHi), mkexpr(tmpEvHi)));
+
+   const HChar* nm[2]       = {"xvhaddw", "xvhsubw"};
+   const HChar* insSizeFrom = mkInsSize(szId);
+   const HChar* insSizeTo =
+      (szId == 3 /* d */) ? "q"
+                          : ((szId == 7 /* du */) ? "qu" : mkInsSize(szId + 1));
+
+   DIP("%s.%s.%s %s, %s, %s\n", nm[isSub], insSizeTo, insSizeFrom, nameXReg(xd),
+       nameXReg(xj), nameXReg(xk));
+
+   STOP_ILL_IF_NO_HWCAP(VEX_HWCAPS_LOONGARCH_LASX);
+
+   putXReg(xd, mkV256from128s(resHi, resLo));
+
+   return True;
+}
+
 static Bool gen_vneg ( DisResult* dres, UInt insn,
                        const VexArchInfo* archinfo,
                        const VexAbiInfo* abiinfo )
@@ -14242,6 +14341,12 @@ static Bool disInstr_LOONGARCH64_WRK_01_1100_0001 ( DisResult* dres, UInt insn,
       case 0b00000:
          ok = gen_vaddw_vsubw_x_x_x(dres, insn, archinfo, abiinfo);
          break;
+      case 0b01010:
+      case 0b01011:
+      case 0b01100:
+      case 0b01101:
+         ok = gen_xvhaddw_xvhsubw(dres, insn, archinfo, abiinfo);
+         break;
       case 0b11000:
       case 0b11001:
       case 0b11010:
@@ -14717,6 +14822,12 @@ static Bool disInstr_LOONGARCH64_WRK_01_1101_0001 ( DisResult* dres, UInt insn,
    switch (SLICE(insn, 21, 17)) {
       case 0b00000:
          ok = gen_xvaddw_xvsubw_x_x_x(dres, insn, archinfo, abiinfo);
+         break;
+      case 0b01010:
+      case 0b01011:
+      case 0b01100:
+      case 0b01101:
+         ok = gen_xvhaddw_xvhsubw(dres, insn, archinfo, abiinfo);
          break;
       case 0b11000:
       case 0b11001:

README.loongarch64
@@ -175,22 +175,6 @@ Unimplemented LSX and LASX instructions (in loongson-community/loongarch-opcodes
 	01_1100_0001_0011001???????????????,vssub.hu,VdVjVk
 	01_1100_0001_0011010???????????????,vssub.wu,VdVjVk
 	01_1100_0001_0011011???????????????,vssub.du,VdVjVk
-	01_1100_0001_0101000???????????????,vhaddw.h.b,VdVjVk
-	01_1100_0001_0101001???????????????,vhaddw.w.h,VdVjVk
-	01_1100_0001_0101010???????????????,vhaddw.d.w,VdVjVk
-	01_1100_0001_0101011???????????????,vhaddw.q.d,VdVjVk
-	01_1100_0001_0101100???????????????,vhsubw.h.b,VdVjVk
-	01_1100_0001_0101101???????????????,vhsubw.w.h,VdVjVk
-	01_1100_0001_0101110???????????????,vhsubw.d.w,VdVjVk
-	01_1100_0001_0101111???????????????,vhsubw.q.d,VdVjVk
-	01_1100_0001_0110000???????????????,vhaddw.hu.bu,VdVjVk
-	01_1100_0001_0110001???????????????,vhaddw.wu.hu,VdVjVk
-	01_1100_0001_0110010???????????????,vhaddw.du.wu,VdVjVk
-	01_1100_0001_0110011???????????????,vhaddw.qu.du,VdVjVk
-	01_1100_0001_0110100???????????????,vhsubw.hu.bu,VdVjVk
-	01_1100_0001_0110101???????????????,vhsubw.wu.hu,VdVjVk
-	01_1100_0001_0110110???????????????,vhsubw.du.wu,VdVjVk
-	01_1100_0001_0110111???????????????,vhsubw.qu.du,VdVjVk
 	01_1100_0001_0111000???????????????,vadda.b,VdVjVk
 	01_1100_0001_0111001???????????????,vadda.h,VdVjVk
 	01_1100_0001_0111010???????????????,vadda.w,VdVjVk
@@ -490,22 +474,6 @@ Unimplemented LSX and LASX instructions (in loongson-community/loongarch-opcodes
 	01_1101_0001_0011001???????????????,xvssub.hu,XdXjXk
 	01_1101_0001_0011010???????????????,xvssub.wu,XdXjXk
 	01_1101_0001_0011011???????????????,xvssub.du,XdXjXk
-	01_1101_0001_0101000???????????????,xvhaddw.h.b,XdXjXk
-	01_1101_0001_0101001???????????????,xvhaddw.w.h,XdXjXk
-	01_1101_0001_0101010???????????????,xvhaddw.d.w,XdXjXk
-	01_1101_0001_0101011???????????????,xvhaddw.q.d,XdXjXk
-	01_1101_0001_0101100???????????????,xvhsubw.h.b,XdXjXk
-	01_1101_0001_0101101???????????????,xvhsubw.w.h,XdXjXk
-	01_1101_0001_0101110???????????????,xvhsubw.d.w,XdXjXk
-	01_1101_0001_0101111???????????????,xvhsubw.q.d,XdXjXk
-	01_1101_0001_0110000???????????????,xvhaddw.hu.bu,XdXjXk
-	01_1101_0001_0110001???????????????,xvhaddw.wu.hu,XdXjXk
-	01_1101_0001_0110010???????????????,xvhaddw.du.wu,XdXjXk
-	01_1101_0001_0110011???????????????,xvhaddw.qu.du,XdXjXk
-	01_1101_0001_0110100???????????????,xvhsubw.hu.bu,XdXjXk
-	01_1101_0001_0110101???????????????,xvhsubw.wu.hu,XdXjXk
-	01_1101_0001_0110110???????????????,xvhsubw.du.wu,XdXjXk
-	01_1101_0001_0110111???????????????,xvhsubw.qu.du,XdXjXk
 	01_1101_0001_0111000???????????????,xvadda.b,XdXjXk
 	01_1101_0001_0111001???????????????,xvadda.h,XdXjXk
 	01_1101_0001_0111010???????????????,xvadda.w,XdXjXk

commit 37fe64242a1721dc20c35494b9378aac426347fd

Add missing handlers for vh{add,sub}w.?[u].?[u]
Fixes: 891fffe3f6cd7234d725b205ccea57d5fa7d3c61

VEX/priv/guest_loongarch64_toIR.c
@@ -14346,7 +14346,7 @@ static Bool disInstr_LOONGARCH64_WRK_01_1100_0001 ( DisResult* dres, UInt insn,
       case 0b01011:
       case 0b01100:
       case 0b01101:
-         ok = gen_xvhaddw_xvhsubw(dres, insn, archinfo, abiinfo);
+         ok = gen_vhaddw_vhsubw(dres, insn, archinfo, abiinfo);
          break;
       case 0b11000:
       case 0b11001: